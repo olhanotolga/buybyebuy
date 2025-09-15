3ade13139c7eddad07902e59fe84e091b9ce97ed
olhanotolga <olha.halat@gmail.com>
2025-09-15 12:12:48 +0200

    ci: change output directory

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const { type } = require('os');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     clean: true,
   },
