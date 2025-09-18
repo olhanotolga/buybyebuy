104c6c955303240ce4f14433652334a65e2e39f9
olhanotolga <olha.halat@gmail.com>
2025-09-18 17:23:42 +0200

    feat: add error boundary

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(
+      error,
+      info.componentStack,
+      React.captureOwnerStack(),
+    );
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/components/BuyByeBuy.jsx
+++ src/components/BuyByeBuy.jsx
@@ -3,14 +3,17 @@ import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import MyProvider from '../context/MyProvider';
 import Router from '../Routes';
+import ErrorBoundary from '../ErrorBoundary';
 
 const BuyByeBuy = () => {
   return (
+    <ErrorBoundary>
     <MyProvider>
       <ThemeProvider theme={theme}>
         <Router />
       </ThemeProvider>
     </MyProvider>
+    </ErrorBoundary>
   );
 };
 
