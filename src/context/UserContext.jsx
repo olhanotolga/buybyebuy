38b1d49af618ab94359d5c1fc51ec6f490cb1cad
olhanotolga <olha.halat@gmail.com>
2025-10-28 15:55:17 +0100

    feat: store user in local storage

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 import { userReducer, ACTION_TYPES } from '../reducers/userReducer';
 
 export const initialUserState = {
@@ -12,6 +12,21 @@ export const initialUserState = {
   },
 };
 
+function getInitialUserState() {
+  try {
+    const userStateLS = localStorage.getItem('userState');
+    return userStateLS
+      ? {
+          users: initialUserState.users,
+          userData: JSON.parse(userStateLS),
+        }
+      : initialUserState;
+  } catch (err) {
+    console.error(err);
+    return initialUserState;
+  }
+}
+
 const UserContext = createContext(null);
 
 export function useUserContext() {
@@ -19,12 +34,30 @@ export function useUserContext() {
 }
 
 function UserProvider({ children }) {
-  const [userState, dispatch] = useReducer(userReducer, initialUserState);
+  const [userState, dispatch] = useReducer(
+    userReducer,
+    initialUserState,
+    getInitialUserState
+  );
+
+  useEffect(() => {
+    const { username, password } = userState.userData;
+    if (username && username !== '' && password && password !== '') {
+      localStorage.setItem(
+        'userState',
+        JSON.stringify({
+          username,
+          password,
+        })
+      );
+    }
+  }, [userState.userData]);
 
   const resetUser = () => {
     dispatch({
-      type: ACTION_TYPES.LOGGED_OUT
-    })
+      type: ACTION_TYPES.LOGGED_OUT,
+    });
+    localStorage.removeItem('userState');
   };
 
   const userContextValue = {
