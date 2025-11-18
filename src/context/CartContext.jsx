6fcd80f9aad59cb4f945390deb6d7d773276e013
olhanotolga <olha.halat@gmail.com>
2025-11-18 17:50:18 +0100

    feat: store cart state in local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 import { CART_ACTION_TYPES, cartReducer } from '../reducers/cartReducer';
+
+const LS_CART_KEY = 'bbbCart';
 
 const CartContext = createContext(null);
 
@@ -8,26 +10,31 @@ export function useCartContext() {
 }
 
 function getInitialCartState() {
-  const lsCart = localStorage.getItem('bbb-cart');
-  return lsCart ? JSON.parse(lsCart) : []
+  try {
+    const lsCart = localStorage.getItem(LS_CART_KEY);
+    return lsCart ? JSON.parse(lsCart) : [];
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 }
 
 function CartProvider({ children }) {
-
-  // shopping cart
   const [cartState, dispatchCart] = useReducer(
-      cartReducer,
-      [],
-      // getInitialCartState
-    );
+    cartReducer,
+    [],
+    getInitialCartState
+  );
 
-  // FUNCTIONS
+  useEffect(() => {
+    localStorage.setItem(LS_CART_KEY, JSON.stringify(cartState));
+  }, [cartState]);
 
   function resetCart() {
     dispatchCart({
-      type: CART_ACTION_TYPES.CART_RESET
-    })
-  };
+      type: CART_ACTION_TYPES.CART_RESET,
+    });
+  }
 
   return (
     <CartContext
